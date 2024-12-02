06313b3cd124095e45e5446f1c1c34113005311e
Jessvpb <160694331+Jessvpb@users.noreply.github.com>
2024-12-02 11:27:27 +0700

    done grid

--- flutter_bottom_navigation/lib/screens/home_screen.dart
+++ flutter_bottom_navigation/lib/screens/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bottom_navigation/data/candi_data.dart';
+import 'package:flutter_bottom_navigation/models/candi.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -9,9 +11,67 @@ class HomeScreen extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Home'),
         ),
-        body: const Padding(
-          padding: EdgeInsets.all(16.0),
-          child: Text('Ini adalah halaman Home'),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              GridView.builder(
+                shrinkWrap: true,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 8,
+                  mainAxisSpacing: 8,
+                ),
+                padding: const EdgeInsets.all(8),
+                itemCount: candiList.length,
+                itemBuilder: (context, index) {
+                  Candi candi = candiList[index];
+                  // Tampilan untuk 1 Grid
+                  return Card(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(16),
+                      ),
+                      margin: const EdgeInsets.all(6),
+                      elevation: 1,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          //GAMBAR CANDI
+                          Expanded(
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(16),
+                              child: Image.asset(
+                                candi.imageAsset,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          ),
+                          //NAMA CANDI
+                          Padding(
+                            padding: const EdgeInsets.only(left: 16, top: 8),
+                            child: Text(
+                              candi.name,
+                              style: const TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                          //TIPE CANDI
+                          Padding(
+                            padding: const EdgeInsets.only(left: 16, bottom: 8),
+                            child: Text(
+                              candi.type,
+                              style: const TextStyle(
+                                fontSize: 12,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ));
+                },
+              )
+            ],
+          ),
         ));
   }
 }
