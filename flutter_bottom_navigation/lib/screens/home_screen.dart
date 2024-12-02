8f342ac83b9f989bfb57f239926d7a32fae91e89
Jessvpb <160694331+Jessvpb@users.noreply.github.com>
2024-12-02 10:32:42 +0700

    update home, search, favorite, profile

---
+++ flutter_bottom_navigation/lib/screens/home_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Home'),
+        ),
+        body: const Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Text('Ini adalah halaman Home'),
+        ));
+  }
+}
